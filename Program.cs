a035f0369757fd22d40a781adee4135c56dc9a14
Wesarat1988 <wesaratnakphairat@gmail.com>
2025-10-03 13:23:07 +0700

    Use absolute URI for PDF API requests

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
