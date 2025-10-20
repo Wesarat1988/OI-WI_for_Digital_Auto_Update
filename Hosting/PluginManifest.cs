354606b5ab04bc99a51f9617c1a6bc7f397b451f
Wesarat1988 <wesaratnakphairat@gmail.com>
2025-10-20 10:09:10 +0700

    Add plugin hosting support to BlazorPdfApp

--- Hosting/PluginManifest.cs
+++ Hosting/PluginManifest.cs
@@ -5,16 +5,12 @@ namespace BlazorPdfApp.Hosting
     public class PluginManifest
     {
         public string Id { get; set; } = string.Empty;
-
         public string Name { get; set; } = string.Empty;
-
         public string Version { get; set; } = string.Empty;
-
         public string Assembly { get; set; } = string.Empty;
-
         public string EntryType { get; set; } = string.Empty;
 
         [JsonIgnore]
-        public string? Folder { get; set; }
+        public string? Folder { get; set; } // set at runtime by loader
     }
 }
